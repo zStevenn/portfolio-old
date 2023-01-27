d224e35146be3e7fd795493aad669ce46e7a041e
zStevenn <steventje678@hotmail.com>
2023-01-27 13:32:05 +0100

    Hero section centered on 3/4 screen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,29 @@
 function Hero() {
 	return (
-		<div
-			className="md:flex md:justify-center md:items-center md:gap-16 px-6 py-4"
-			id="hero"
-		>
-			<div className="flex flex-col justify-center align-center text-slate-50 py-8">
-				<p className="text-2xl">Hey!</p>
-				<h1 className="text-3xl tracking-wide">
-					Ik ben <strong className="text-red-800">Steven Li,</strong>
-				</h1>
-				<h1 className="text-3xl tracking-wide">Softwaredeveloper</h1>
-				<div className="flex gap-8">
-					<a
-						href="#contact"
-						className="px-1 py-2 bg-red-500 border-b-2 border-r-2 border-red-800 text-slate-50 text-lg"
+		<div className="grid grid-cols-1 place-items-center min-h-[75vh]">
+
+		<div className="text-slate-50 px-6 text-center" id="hero">
+			<p className="text-2xl mb-2">Hey!</p>
+			<h1 className="text-3xl tracking-wide mb-2">
+				Ik ben <strong className="text-red-800">Steven Li,</strong>
+			</h1>
+			<h1 className="text-3xl tracking-wide mb-4">Softwaredeveloper</h1>
+			<div className="flex justify-center gap-8">
+				<a
+					href="#contact"
+					className="px-1 py-2 bg-red-500 border-b-2 border-r-2 border-red-800 text-slate-50 text-lg"
 					>
-						Contact
-					</a>
-					<a
-						href="#projecten"
-						className="px-1 py-2 bg-gray-500 border-b-2 border-r-2 border-gray-800 text-slate-50 text-lg"
+					Contact
+				</a>
+				<a
+					href="#projecten"
+					className="px-1 py-2 bg-gray-500 border-b-2 border-r-2 border-gray-800 text-slate-50 text-lg"
 					>
-						Projecten
-					</a>
-				</div>
+					Projecten
+				</a>
 			</div>
 		</div>
+					</div>
 	)
 }
 
