3be8806fa89766408c2650ca883d1c59d075209d
zStevenn <steventje678@hotmail.com>
2023-01-27 14:48:41 +0100

    Changes handleClick

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
 	// Array of nav items with id and name.
 	const navElements = [
-		{ id: "", name: "Home" },
+		{ id: "hero", name: "Home" },
 		{ id: "overmij", name: "Over Mij" },
 		{ id: "contact", name: "Contact" },
 		{ id: "services", name: "Services" },
@@ -31,12 +31,27 @@ const Navbar = () => {
 		}
 	}, [])
 
-	// Function to cancel default behavior and enable smooth scrolling
 	function handleClick(event, id) {
 		event.preventDefault()
 		const element = document.getElementById(id)
-		element.scrollIntoView({ behavior: "smooth" })
+
+		// Add class to element to scroll to
+		element.classList.add("pt-[10vh]")
+
+		// Scroll to element
+		element.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+			inline: "nearest",
+		})
+
+		// Remove class after 1 second
+		setTimeout(() => {
+			element.classList.remove("pt-[10vh]");
+		}, 1000);
+		
 	}
+	
 
 	// Toggles Navigation based on click
 	const toggleNav = () => {
@@ -47,13 +62,18 @@ const Navbar = () => {
 		<>
 			{/* Navbar */}
 			<nav
-				className={`flex justify-between items-center px-6 py-2 min-h-[10vh] z-50 bg-neutral-900 ${
+				id="navbar"
+				className={`flex justify-between items-center px-8 py-2 min-h-[10vh] z-50 bg-neutral-800 ${
 					isSticky ? "fixed top-0 w-full" : ""
 				}`}
 			>
 				{/* Hamburger Icon */}
 				<span className="lg:hidden text-slate-50 text-xl" onClick={toggleNav}>
-					{showNav ? <MdClose className="text-3xl" /> : <MdMenu  className="text-3xl"/>}
+					{showNav ? (
+						<MdClose className="text-3xl" />
+					) : (
+						<MdMenu className="text-3xl" />
+					)}
 				</span>
 				{/* Webpage Title */}
 				<h1 className="text-slate-50 text-xl">Steven Li</h1>
@@ -61,7 +81,9 @@ const Navbar = () => {
 				<ul className="hidden lg:flex gap-8 text-slate-50 text-lg">
 					{navElements.map(({ id, name }) => (
 						<li key={id}>
-							<a href={`#${id}`}>{name}</a>
+							<a href="#" onClick={e => handleClick(e, id)}>
+								{name}
+							</a>
 						</li>
 					))}
 				</ul>
@@ -76,11 +98,13 @@ const Navbar = () => {
 			{showNav && (
 				<>
 					<ul
-						className={`text-slate-50 px-8 py-4 fixed top-[10vh] right-0 left-0 bg-neutral-900 border-bottom`}
+						className={`text-slate-50 px-8 py-4 fixed top-[10vh] right-0 left-0 bg-neutral-800 border-bottom z-50`}
 					>
 						{navElements.map(({ id, name }) => (
 							<li className="my-2" key={id} onClick={toggleNav}>
-								<a href={`#${id}`}>{name}</a>
+								<a href="#" onClick={e => handleClick(e, id)}>
+									{name}
+								</a>
 							</li>
 						))}
 					</ul>
