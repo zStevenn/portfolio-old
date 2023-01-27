92149eb69a4c8a42d59112f7cc6e9a335e7e444b
zStevenn <steventje678@hotmail.com>
2023-01-27 12:00:57 +0100

    Imports outfit into the application

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import "@fontsource/outfit"; // Defaults to weight 400.
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
