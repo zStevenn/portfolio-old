1703471fc37cdc00dba4dcc30e912676ef7174bd
zStevenn <steventje678@hotmail.com>
2023-01-27 14:01:37 +0100

    Refactoring to Hero Adds a profile picture to homepage Places hero section in a grid Adds spacing between items

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,31 @@
+import HeroImage from "../assets/svg/profile.svg"
+
 function Hero() {
 	return (
-		<div className="grid grid-cols-1 place-items-center min-h-[75vh]">
-
-		<div className="text-slate-50 px-6 text-center" id="hero">
-			<p className="text-2xl mb-2">Hey!</p>
-			<h1 className="text-3xl tracking-wide mb-2">
-				Ik ben <strong className="text-red-800">Steven Li,</strong>
-			</h1>
-			<h1 className="text-3xl tracking-wide mb-4">Softwaredeveloper</h1>
-			<div className="flex justify-center gap-8">
-				<a
-					href="#contact"
-					className="px-1 py-2 bg-red-500 border-b-2 border-r-2 border-red-800 text-slate-50 text-lg"
+		<div className="grid grid-cols-1 md:grid-cols-2 gap-8 place-items-center px-8 py-16">
+			<div className="text-slate-50 text-center" id="hero">
+				<p className="text-2xl mb-2">Hey!</p>
+				<h1 className="text-3xl tracking-wide mb-2">
+					Ik ben <strong className="text-red-800">Steven Li,</strong>
+				</h1>
+				<h1 className="text-3xl tracking-wide mb-4">Software developer.</h1>
+				<div className="flex justify-center gap-8">
+					<a
+						href="#contact"
+						className="px-1 py-2 bg-red-500 border-b-2 border-r-2 border-red-800 text-slate-50 text-lg"
 					>
-					Contact
-				</a>
-				<a
-					href="#projecten"
-					className="px-1 py-2 bg-gray-500 border-b-2 border-r-2 border-gray-800 text-slate-50 text-lg"
+						Contact
+					</a>
+					<a
+						href="#projecten"
+						className="px-1 py-2 bg-gray-500 border-b-2 border-r-2 border-gray-800 text-slate-50 text-lg"
 					>
-					Projecten
-				</a>
+						Projecten
+					</a>
+				</div>
 			</div>
+			<img src={HeroImage} alt="Steven Li" />
 		</div>
-					</div>
 	)
 }
 
