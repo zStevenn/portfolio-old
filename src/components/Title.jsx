e7716fe7cd8461286ae37945e6c58bc8581889eb
zStevenn <steventje678@hotmail.com>
2023-02-01 15:32:37 +0100

    Creates Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,9 @@
+const Title = ({ text }) => {
+	return (
+		<h1 className="text-slate-50 text-center text-2xl font-semibold pt-4 pb-8">
+			<span className="text-slate-200 pr-2">#</span>{text}
+		</h1>
+	)
+}
+
+export default Title
