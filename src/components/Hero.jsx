1e969d165087fba21de44adbb5d2168c606b3c7d
zStevenn <steventje678@hotmail.com>
2023-04-03 20:57:23 +0200

    Styled buttons rounded

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,13 +18,13 @@ function Hero() {
 					<a
 						download={"Steven Li CV"}
 						href="/src/assets/static/Steven_Li_CV_NL.pdf"
-						className="px-1 py-2 bg-red-500 border-b-2 border-r-2 border-red-800 text-slate-50 text-lg"
+						className="px-2 py-2 rounded bg-red-500 border-b-2 border-r-2 border-red-800 text-slate-50 text-lg"
 					>
 						Download CV
 					</a>
 					<a
 						href="#projecten"
-						className="px-1 py-2 bg-gray-500 border-b-2 border-r-2 border-gray-800 text-slate-50 text-lg"
+						className="px-2 py-2 rounded bg-gray-500 border-b-2 border-r-2 border-gray-800 text-slate-50 text-lg"
 					>
 						Projecten
 					</a>
