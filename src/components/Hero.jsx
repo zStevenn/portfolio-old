5b5ea99c892d07927baca031cf4a307938f8313e
Steven <steventje678@hotmail.com>
2023-04-03 16:10:49 +0200

    Aligns content on the left

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,18 @@ import HeroImage from "../assets/static/profile.png"
 function Hero() {
 	return (
 		<div className="grid grid-cols-1 gap-8 md:grid-cols-2 place-items-center py-16 h-screen" id="hero">
-			<div className="text-slate-50 text-center">
+			<div className="text-slate-50 w-full">
 				<p className="text-2xl md:text-3xl xl:text-5xl mb-2">Hey!</p>
 				<h1 className="text-3xl md:text-4xl xl:text-6xl tracking-wide mb-2">
 					Ik ben <strong className="text-red-800">Steven Li,</strong>
 				</h1>
 				<h1 className="text-3xl md:text-4xl xl:text-6xl tracking-wide mb-4">Softwaredeveloper.</h1>
-				<div className="flex justify-center gap-8">
+				<div className="flex justify-start gap-8">
 					<a
-						href="#contact"
+						href="#"
 						className="px-1 py-2 bg-red-500 border-b-2 border-r-2 border-red-800 text-slate-50 text-lg"
 					>
-						Contact
+						Download CV
 					</a>
 					<a
 						href="#projecten"
@@ -24,7 +24,7 @@ function Hero() {
 					</a>
 				</div>
 			</div>
-			<img src={HeroImage} className="grayscale" alt="Steven Li" />
+			<img src={HeroImage} alt="Steven Li" />
 		</div>
 	)
 }
