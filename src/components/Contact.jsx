67d31662aedaddcba350d49829c001b25e3f489e
Steven <steventje678@hotmail.com>
2023-04-03 16:36:47 +0200

    Changes to styling
    Removes the use of Title
    Refactors the form into white with black letters and a red submit button
    Removes previously written text
    Adds contact details and a list of socials

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,57 +1,68 @@
-import Title from "./Title"
 import { MdOutlineMail } from "react-icons/md"
-import { BsLinkedin, BsWhatsapp } from "react-icons/bs"
+import { BsLinkedin, BsWhatsapp, BsGithub, BsDiscord } from "react-icons/bs"
 
 function Contact() {
 	return (
-		<div className="pb-40" id="contact">
-			<Title text="Contact" />
+		<div className="my-24 bg-neutral-900" id="contact">
+			<h1 className="text-slate-50 text-2xl font-semibold py-4"># Contact</h1>
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-content-center gap-8">
 				<div className="text-slate-50 lg:col-span-2">
 					<p className="tracking-wide mb-4">
-						Bedankt dat je mijn contactpagina bezoekt! Als je vragen hebt of
-						interesse hebt in mijn werk, aarzel dan niet om contact met me op te
-						nemen. Je kunt me bereiken via e-mail of via een van de social
-						media-links hieronder. Ik kijk ernaar uit om van je te horen!
+						Steven Li
+						</br>
+						Driebergen-Rijsenburg
+						</br>
+						Utrecht, Nederland
+						</br></br>
+						<a href="mailto:stevenli.inbox@gmail.com" target="_blank"><MdOutlineMail className="inline mr-2" /> stevenli.inbox@gmail.com</a>
+						</br>
+						<BsWhatsapp className="inline mr-2" /> (06) - 409 17 668
 					</p>
-					<ul>
-						<li>
-							<a href="mailto:stevenli.inbox@gmail.com" target="_blank">
-								<MdOutlineMail className="inline mr-2" />
-								stevenli.inbox@gmail.com
+					<ul className="flex flex-row justify-between flex-nowrap gap-8">
+						<li className="rounded p-4 hover:scale-110">
+							<a href="https://www.linkedin.com/in/zstevenn/" target="_blank">
+								<BsLinkedin className="inline mr-2" />
 							</a>
 						</li>
-						<li>
-							<BsWhatsapp className="inline mr-2" />
-							(06) - 409 17 668
+						<li className="rounded p-4 hover:scale-110">
+							<a href="https://github.com/zStevenn" target="_blank">
+								<BsGithub className="inline mr-2" />
+							</a>
 						</li>
-						<li>
-							<a href="https://www.linkedin.com/in/zstevenn/" target="_blank">
-								<BsLinkedin className="inline mr-2" /> LinkedIn
+						<li className="rounded p-4 hover:scale-110">
+							<a href="https://github.com/zStevenn" target="_blank">
+								<BsGithub className="inline mr-2" />
+							</a>
+						</li>
+						<li className="rounded p-4 hover:scale-110">
+							<a href="https://github.com/zStevenn" target="_blank">
+								<BsGithub className="inline mr-2" />
 							</a>
 						</li>
 					</ul>
 				</div>
-				<form className="bg-red-500 p-6">
-					<h3 className="text-white text-lg font-semibold text-center">
-						Contactformulier
+				<div className="text-slate-50 text-2xl text-center">Of</div>
+	
+				<form className="bg-neutral-50 p-6 rounded">
+					<h3 className="text-neutral-900 text-lg font-semibold text-center">
+						Stuur een bericht
 					</h3>
-					<label className="block text-slate-50 mt-2">Naam</label>
+					<label className="block text-neutral-900 mt-2">Naam</label>
 					<input
 						type="text"
-						className="bg-red-300 border-b-2 border-r-2 border-red-800 text-slate-50 mb-2 w-full"
+						className="bg-neutral-400 border-b-2 border-r-2 border-neutral-800 rounded text-slate-50 mb-2 w-full"
 					/>
-					<label className="block text-slate-50 mt-2">Emailadres</label>
+					<label className="block text-neutral-900 mt-2">Email</label>
 					<input
 						type="text"
-						className="bg-red-300 border-b-2 border-r-2 border-red-800 text-slate-50 mb-2 w-full"
+						className="bg-neutral-400 border-b-2 border-r-2 border-neutral-800 rounded text-slate-50 mb-2 w-full"
 					/>
-					<label className="block text-slate-50 mt-2">Bericht</label>
-					<textarea className="bg-red-300 border-b-2 border-r-2 border-red-800 text-slate-50 mb-2 w-full" />
+					<label className="block text-neutral-900 mt-2">Bericht</label>
+					<textarea className="bg-neutral-400 border-b-2 border-r-2 border-neutral-800 rounded text-slate-50 mb-2 w-full" />
 					<button
 						type="button"
 						value="submit"
-						className="block px-1 py-2 bg-gray-500 border-b-2 border-r-2 border-gray-800 text-slate-50 mx-auto"
+						className="block px-4 py-2 bg-red-500 border-b-2 border-r-2 border-red-800 rounded text-slate-50 mx-auto"
 					>
 						Verstuur bericht
 					</button>
