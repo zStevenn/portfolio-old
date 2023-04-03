4065335cb079a11aec5983deb609bddd47e82057
Steven <steventje678@hotmail.com>
2023-04-03 16:45:11 +0200

    Refactors styling
    Makes background white and text dark

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,29 @@
 import { SiDiscord } from "react-icons/si"
-import Title from "./Title"
 import Project1 from "../assets/static/cafetaria-de-sluis.png"
 
 function Projects() {
 	return (
 		<div
-			className="mb-40"
+			className="my-24 bg-neutral-50"
 			id="projecten"
 		>
-			<Title text="Projecten" />
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 place-content-center gap-8">
-				<div className="flex flex-col">
-					<h3 className="text-slate-50 text-center my-4 text-xl">
+			<h1 className="text-neutral-900 text-2xl"># Projecten</h1>
+			<div className="grid grid-cols-1 md:grid-cols-2 place-content-center gap-8">
+				<div className="grid">
+					<h3 className="text-neutral-900 my-4 text-xl">
 						Cafetaria de Sluis
 					</h3>
 					<a href="https://cafetariadesluis.nl" target="_blank">
-						<img src={Project1} />
+						<img src={Project1} title="Cafetaria de Sluis" alt="Cafetaria de Sluis" />
 					</a>
 				</div>
-				<div className="flex flex-col items-center">
-					<h3 className="text-slate-50 text-center my-4 text-xl">
-						Discord Bot
+				<div className="grid">
+					<h3 className="text-neutral-900 my-4 text-xl">
+						Steven's To-Do
 					</h3>
-					<SiDiscord className="text-8xl text-slate-50" />
+					<a href="https://stevens-todo-app.netlify.app" target="_blank">
+						<img src={Project1} title="Steven's To-Do App" alt="Steven's To-Do App" />
+					</a>
 				</div>
 			</div>
 		</div>
