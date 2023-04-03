24ab215c68ec024549ac2c74c94aae37835002d8
Steven <steventje678@hotmail.com>
2023-04-03 16:21:59 +0200

    Refactoring to Skills
    Makes the whole section background white.
    Adds additional about me text.
    Changes ID to 'overmij'
    Changes the tiles to Figma, Netlify, React and Tailwind.
    Removes the use of 'Title'

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,40 +1,37 @@
-import Title from "./Title"
 import {
-	SiCss3,
-	SiHtml5,
-	SiJavascript,
-	SiPhp,
 	SiReact,
 	SiTailwindcss,
+	SiFigma,
+	SiNetlify,
 } from "react-icons/si"
 
 function Skills() {
 	return (
-		<div className="mb-40" id="vaardigheden">
-			<Title text="Vaardigheden" />
-			<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-				<div className="flex flex-col justify-center items-center bg-red-500 border-b-2 border-r-2 border-red-800 px-3 py-6 shadow shadow-red-800 hover:shadow-md">
-					<h3 className="text-slate-50 pb-4">HTML5</h3>
-					<SiHtml5 className="text-8xl text-slate-50" />
+		<div className="my-24 bg-white" id="overmij">
+			<h1 className="text-neutral-900"># Over mij</h1>
+			<ul className="list-disc">
+				<li>Momenteel bezig met mijn opleiding Softwaredeveloper aan MBO Utrecht waar ik mijn diploma verwacht in Juni 2023.</li>
+				<li>Mijn doel is om innovatieve en effectieve oplossingen te creëren.</li>
+				<li>Ik ben een nauwkeurige probleemoplosser en effectieve teamspeler.</li>
+				<li>Altijd op zoek naar nieuwe manieren om mijn vaardigheden te verbeteren en kennis uit te breiden.</li>
+				<li>Ervaring in HTML, CSS, JavaScript, PHP en UI/UX design.</li>
+			</ul>
+			<p className="text-neutral-900">Gewerkt met programma's, libraries en frameworks zoals:<p>
+			<div className="grid grid-cols-2 gap-8">
+				<div className="grid place-items-center bg-neutral-900 border-b-2 border-r-2 border-neutral-800 px-3 py-6 shadow shadow-neutral-800 hover:shadow-md">
+					<h3 className="text-neutral-50 pb-4">Figma</h3>
+					<SiFigma className="text-8xl text-slate-50" />
 				</div>
-				<div className="flex flex-col justify-center items-center bg-red-500 border-b-2 border-r-2 border-red-800 px-3 py-6 shadow shadow-red-800 hover:shadow-md">
-					<h3 className="text-slate-50 pb-4">CSS</h3>
-					<SiCss3 className="text-8xl text-slate-50" />
+				<div className="grid place-items-center bg-neutral-900 border-b-2 border-r-2 border-neutral-800 px-3 py-6 shadow shadow-neutral-800 hover:shadow-md">
+					<h3 className="text-neutral-50 pb-4">Netlify</h3>
+					<SiNetlify className="text-8xl text-slate-50" />
 				</div>
-				<div className="flex flex-col justify-center items-center bg-red-500 border-b-2 border-r-2 border-red-800 px-3 py-6 shadow shadow-red-800 hover:shadow-md">
-					<h3 className="text-slate-50 pb-4">Javascript</h3>
-					<SiJavascript className="text-8xl text-slate-50" />
-				</div>
-				<div className="flex flex-col justify-center items-center bg-red-500 border-b-2 border-r-2 border-red-800 px-3 py-6 shadow shadow-red-800 hover:shadow-md">
-					<h3 className="text-slate-50 pb-4">PHP</h3>
-					<SiPhp className="text-8xl text-slate-50" />
-				</div>
-				<div className="flex flex-col justify-center items-center bg-red-500 border-b-2 border-r-2 border-red-800 px-3 py-6 shadow shadow-red-800 hover:shadow-md">
-					<h3 className="text-slate-50 pb-4">React</h3>
+				<div className="grid place-items-center bg-neutral-900 border-b-2 border-r-2 border-neutral-800 px-3 py-6 shadow shadow-neutral-800 hover:shadow-md">
+					<h3 className="text-neutral-50 pb-4">React</h3>
 					<SiReact className="text-8xl text-slate-50" />
 				</div>
-				<div className="flex flex-col justify-center items-center bg-red-500 border-b-2 border-r-2 border-red-800 px-3 py-6 shadow shadow-red-800 hover:shadow-md">
-					<h3 className="text-slate-50 pb-4">Tailwind</h3>
+				<div className="grid place-items-center bg-neutral-900 border-b-2 border-r-2 border-neutral-800 px-3 py-6 shadow shadow-neutral-800 hover:shadow-md">
+					<h3 className="text-neutral-50 pb-4">Tailwind</h3>
 					<SiTailwindcss className="text-8xl text-slate-50" />
 				</div>
 			</div>
