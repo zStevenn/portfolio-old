4f68e498173ac7b9eabc7d81859d477bef210469
zStevenn <steventje678@hotmail.com>
2023-04-08 14:20:37 +0200

    Adds constraint to image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import HeroImage from "../assets/static/profile.png"
 
 function Hero() {
 	return (
-		<div className="grid grid-cols-1 gap-8 md:grid-cols-2 place-items-center py-16 h-screen" id="hero">
+		<div className="grid grid-cols-1 gap-8 sm:grid-cols-2 place-items-center py-16 h-screen" id="hero">
 			<div className="text-slate-50 text-center">
 				<p className="text-2xl md:text-3xl xl:text-5xl mb-2">Hey!</p>
 				<h1 className="text-3xl md:text-4xl xl:text-6xl tracking-wide mb-2">
@@ -12,19 +12,19 @@ function Hero() {
 				<div className="flex justify-center gap-8">
 					<a
 						href="#contact"
-						className="px-1 py-2 bg-red-500 border-b-2 border-r-2 border-red-800 text-slate-50 text-lg"
+						className="px-3 py-2 bg-red-500 border-b-2 border-r-2 border-red-900 rounded text-slate-50 text-lg"
 					>
 						Contact
 					</a>
 					<a
 						href="#projecten"
-						className="px-1 py-2 bg-gray-500 border-b-2 border-r-2 border-gray-800 text-slate-50 text-lg"
+						className="px-3 py-2 bg-gray-500 border-b-2 border-r-2 border-gray-900 rounded text-slate-50 text-lg"
 					>
 						Projecten
 					</a>
 				</div>
 			</div>
-			<img src={HeroImage} className="grayscale" alt="Steven Li" />
+			<img src={HeroImage} className="grayscale max-w-xs" alt="Steven Li" />
 		</div>
 	)
 }
